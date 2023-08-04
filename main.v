707158d4d6c836da1e09d936905432aa5965639e
Eliyaan <103932369+Eliyaan@users.noreply.github.com>
2023-08-04 22:27:36 +0200

    Works :blush:

--- main.v
+++ main.v
@@ -6,13 +6,14 @@ import math.bits
 
 
 const (
-    win_width    = 901
-    win_height   = 901
+    win_width    = 941
+    win_height   = 941
     bg_color     = gx.black
 
 	
     pixel_size = 2
-	nb_tiles = 50
+	nb_tiles = 170
+	refresh = 11000
 	sim_size = pixel_size * nb_tiles
     text_cfg = gx.TextCfg{color: gx.green, size: 20, align: .left, vertical_align: .top}
 )
@@ -48,6 +49,8 @@ struct App {
 mut:
     gg    &gg.Context = unsafe { nil }
 	tiles_states [][]u8 = [][]u8{len:nb_tiles, cap:nb_tiles, init:[]u8{len:nb_tiles, cap:nb_tiles, init:u8(0)}}  // [Ligne][colonne]
+	water_tiles_coords [][]int 
+	wall_tiles_coords [][]int 
 }
 
 
@@ -73,12 +76,14 @@ fn main() {
 	mut y := 0
 	for i, mut line in app.tiles_states{
 		for j, mut tile in line{
-			tile = int(custom_int_in_range(0,20)/10)
+			tile = int(custom_int_in_range(0,28)/10)
 			if tile == 0{
 				w += 1
 			}else if tile == 1{
+				app.water_tiles_coords << [[i, j]]
 				x += 1
 			}else if tile == 2{
+				app.wall_tiles_coords << [[i, j]]
 				y += 1
 			}
 		}
@@ -92,55 +97,53 @@ fn main() {
 [direct_array_access]
 fn on_frame(mut app App) {
 	//Process
-
-	mut water := 0
-	for i := app.tiles_states.len-1; i >= 0; i-- {
-		mut rand_tile_list := []int{len:nb_tiles, init:index}
-		for k in 0..nb_tiles{  // 		for j in 0..app.tiles_states[i].len{
-			mut rand_index := custom_int_in_range(0, rand_tile_list.len-k)
-			for rand_tile_list[rand_index] == -1{
-				rand_index += 1
-			}
-			j := rand_tile_list[rand_index]
-			rand_tile_list[rand_index] = -1
-			if app.tiles_states[i][j] == 1{
-				if i != app.tiles_states.len-1 && app.tiles_states[i+1][j] == 0{
-					app.tiles_states[i][j]= 0
-					app.tiles_states[i+1][j] = 1
-					water += 1
-				}else{
-					if  j != nb_tiles-1 && app.tiles_states[i][j+1] == 0{ // droite libre (+)
-						if j != 0 && app.tiles_states[i][j-1] == 0{ // deux cotés libres
-							if custom_int_in_range(0,2) == 0{
-								app.tiles_states[i][j]= 0
-								app.tiles_states[i][j+1] = 1
-								water += 1
-							}else{
-								app.tiles_states[i][j]= 0
-								app.tiles_states[i][j-1] = 1
-								water += 1
-							}
-						}else{ // que le droite (+)
-							if custom_int_in_range(0,5) == 0{// si on enlevais le random ca coulerai tout le temps vers le coté libre
-								app.tiles_states[i][j]= 0
-								app.tiles_states[i][j+1] = 1
-								water += 1
-							}
-						}
-					}else{ // pas le droite (+)
-						if j != 0 && app.tiles_states[i][j-1] == 0{  // que le gauche (-)
-							if custom_int_in_range(0,2) == 0{// si on enlevais le random ca coulerai tout le temps vers le coté libre
-								app.tiles_states[i][j]= 0
-								app.tiles_states[i][j-1] = 1
-								water += 1
-							}
+	mut water_updates := 0
+	for mut w_coo in app.water_tiles_coords{
+		i := w_coo[0]
+		j := w_coo[1]
+		if app.tiles_states[i][j] == 1{
+			if i != app.tiles_states.len-1 && app.tiles_states[i+1][j] == 0{
+				app.tiles_states[i][j]= 0
+				app.tiles_states[i+1][j] = 1
+				w_coo[0] += 1
+				water_updates+= 1
+			}else{
+				if  j != nb_tiles-1 && app.tiles_states[i][j+1] == 0{ // droite libre (+)
+					if j != 0 && app.tiles_states[i][j-1] == 0{ // deux cotés libres
+						if custom_int_in_range(0,2) == 0{
+							app.tiles_states[i][j]= 0
+							app.tiles_states[i][j+1] = 1
+							w_coo[1] += 1
+							water_updates+= 1
 						}else{
-							//Aucun des deux
-							if i != 0 && app.tiles_states[i-1][j] == 0 && custom_int_in_range(0,10) == 0{
-								app.tiles_states[i][j]= 0
-								app.tiles_states[i-1][j] = 1
-								water += 1
-							}
+							app.tiles_states[i][j]= 0
+							app.tiles_states[i][j-1] = 1
+							w_coo[1] -= 1
+							water_updates+= 1
+						}
+					}else{ // que le droite (+)
+						if custom_int_in_range(0,2) == 0{// si on enlevais le random ca coulerai tout le temps vers le coté libre
+							app.tiles_states[i][j]= 0
+							app.tiles_states[i][j+1] = 1
+							w_coo[1] += 1
+							water_updates+= 1
+						}
+					}
+				}else{ // pas le droite (+)
+					if j != 0 && app.tiles_states[i][j-1] == 0{  // que le gauche (-)
+						if custom_int_in_range(0,2) == 0{// si on enlevais le random ca coulerai tout le temps vers le coté libre
+							app.tiles_states[i][j]= 0
+							app.tiles_states[i][j-1] = 1
+							w_coo[1] -= 1
+							water_updates+= 1
+						}
+					}else{
+						//Aucun des deux
+						if i != 0 && app.tiles_states[i-1][j] == 0 && custom_int_in_range(0,5) == 0{
+							app.tiles_states[i][j]= 0
+							app.tiles_states[i-1][j] = 1
+							w_coo[0] -= 1
+							water_updates+= 1
 						}
 					}
 				}
@@ -149,17 +152,30 @@ fn on_frame(mut app App) {
 	}
 
     //Draw
-	for i, line in app.tiles_states{
-		app.gg.begin()
-		for j, tile in line{
-			app.gg.draw_rect_filled(j*pixel_size+30, i*pixel_size+20, pixel_size, pixel_size, color(tile))
+	app.gg.begin()
+	app.gg.draw_rect_filled(30, 20, pixel_size*nb_tiles, pixel_size*nb_tiles, color(0))
+
+	for i, water_tile in app.water_tiles_coords{
+		app.gg.draw_rect_filled(water_tile[1]*pixel_size+30, water_tile[0]*pixel_size+20, pixel_size, pixel_size, gx.Color{66, 135, 245, 255})
+		if i%refresh == refresh-1{
+			app.gg.end(how: .passthru)
+			app.gg.begin()
 		}
-		app.gg.end(how: .passthru)
 	}
+	app.gg.end(how: .passthru)
+	app.gg.begin()
+	for i, wall_tile in app.wall_tiles_coords{
+		app.gg.draw_rect_filled(wall_tile[1]*pixel_size+30, wall_tile[0]*pixel_size+20, pixel_size, pixel_size, gx.black)
+		if i%refresh == refresh-1{
+			app.gg.end(how: .passthru)
+			app.gg.begin()
+		}
+	}
+	
 	app.gg.begin()
 	app.gg.show_fps()
-	app.gg.draw_rect_filled(40, 0, 200, 20, gx.black)
-	app.gg.draw_text(40, 0, "Nb water updates: ${water}", text_cfg)
+	app.gg.draw_rect_filled(40, 0, 220, 20, gx.black)
+	app.gg.draw_text(40, 0, "Nb water updates: ${water_updates}", text_cfg)
 	app.gg.end(how: .passthru)
 }
 
