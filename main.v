7e376c092b7f5201a53d314e63d4f90ba84f4cf5
Eliyaan <103932369+Eliyaan@users.noreply.github.com>
2023-08-05 18:56:14 +0200

    Premier logiciel qui marche nicely :D

--- main.v
+++ main.v
@@ -1,8 +1,6 @@
 module main
 //check si y'a de l'air dans les lignes proches pour voir si update requise ou pas
 // implementer les nouveaux déplacements
-// trouver pk ca skip des particules ? #help ?
-// nouveau syst de rendu sinon avec rect 
 
 
 import sokol.sgl
@@ -13,25 +11,26 @@ import math.bits
 import math
 
 const (
-    win_width    = 1041
-    win_height   = 1041
+    win_width    = 740
+    win_height   = 740
     bg_color     = gx.black
 
 	
     pixel_size = 1
-	nb_tiles = 700
+	nb_tiles = 680
 	refresh = 65000
 	sim_size = pixel_size * nb_tiles
     text_cfg = gx.TextCfg{color: gx.green, size: 20, align: .left, vertical_align: .top}
 	x_offset = 30
 	y_offset = 20
+	blue = u32(0xFF4287F5)
 )
 
 [inline]
 fn color(nb u8) gx.Color{
 	return match nb{
 		0 {gx.white}
-    	1 {gx.Color{66, 135, 245, 255}}
+    	1 {gx.Color{66, 135, 245, 255}}  // 0x4287F5FF
 		2 {gx.black}
 		else{gx.red}
 	}
@@ -57,13 +56,15 @@ fn custom_int_in_range(min int, max int) int{
 struct App {
 mut:
     gg    &gg.Context = unsafe { nil }
-	tiles_states [][]u8 = [][]u8{len:nb_tiles, cap:nb_tiles, init:[]u8{len:nb_tiles, cap:nb_tiles, init:u8(0)}}  // [Ligne][colonne]
+	tiles_states [nb_tiles][nb_tiles]u8 = [nb_tiles][nb_tiles]u8{init:[nb_tiles]u8{ init:u8(0)}}  // [Ligne][colonne]
+	istream_idx int
+	screen_pixels [nb_tiles][nb_tiles]u32 = [nb_tiles][nb_tiles]u32{init:[nb_tiles]u32{ init:u32(0xFFFF_FFFF)}}
 	water_tiles_coords [][]int 
 	wall_tiles_coords [][]int 
 	mouse_held bool
 	mouse_coords [2]int 
 	paint_type int = 1
-	paint_size int = 6
+	paint_size int = 20
 }
 
 
@@ -75,10 +76,10 @@ fn main() {
         width: win_width
         height: win_height
         create_window: true
-		fullscreen: true
-        window_title: '- Application -'
+        window_title: 'Water/Sand simutation'
         user_data: app
         bg_color: bg_color
+		init_fn: graphics_init
         frame_fn: on_frame
         event_fn: on_event
     )
@@ -120,6 +121,7 @@ fn on_frame(mut app App) {
 					if app.mouse_coords[0]+c < nb_tiles && app.mouse_coords[0]+c >= 0 && app.mouse_coords[1]+l < nb_tiles && app.mouse_coords[1]+l >= 0 {
 						if app.paint_type == 1 && app.tiles_states[app.mouse_coords[1]+l][app.mouse_coords[0]+c] == 0{
 							app.tiles_states[app.mouse_coords[1]+l][app.mouse_coords[0]+c] = 1
+							app.screen_pixels[app.mouse_coords[1]+l][app.mouse_coords[0]+c] = blue
 							if app.water_tiles_coords.len == 0 {
 								app.water_tiles_coords << [[app.mouse_coords[1]+l, app.mouse_coords[0]+c]]
 							}else{
@@ -130,6 +132,7 @@ fn on_frame(mut app App) {
 							}
 						}else if app.paint_type == 2 && app.tiles_states[app.mouse_coords[1]+l][app.mouse_coords[0]+c] == 0{
 							app.tiles_states[app.mouse_coords[1]+l][app.mouse_coords[0]+c] = 2
+							app.screen_pixels[app.mouse_coords[1]+l][app.mouse_coords[0]+c] = 0x0000_00FF
 							app.wall_tiles_coords << [[app.mouse_coords[1]+l, app.mouse_coords[0]+c]]
 						}
 					}
@@ -146,17 +149,23 @@ fn on_frame(mut app App) {
 			if i != app.tiles_states.len-1 && app.tiles_states[i+1][j] == 0{ // Haut
 				if  j != nb_tiles-1 && app.tiles_states[i][j+1] == 0{ // droite libre (+)
 					if j != 0 && app.tiles_states[i][j-1] == 0{ // deux cotés libres + haut = descendre
-						app.tiles_states[i][j]= 0
+						app.tiles_states[i][j] = 0
+						app.screen_pixels[i][j] = 0xFFFF_FFFF
 						app.tiles_states[i+1][j] = 1
+						app.screen_pixels[i+1][j] = blue
 						w_coo[0] += 1
 					}else{ // que le droite + haut
 						if custom_int_in_range(0,2) == 0{
 							app.tiles_states[i][j]= 0
+							app.screen_pixels[i][j] = 0xFFFF_FFFF
 							app.tiles_states[i][j+1] = 1
+							app.screen_pixels[i][j+1] = blue
 							w_coo[1] += 1
 						}else{
 							app.tiles_states[i][j]= 0
+							app.screen_pixels[i][j] = 0xFFFF_FFFF
 							app.tiles_states[i+1][j] = 1
+							app.screen_pixels[i+1][j] = blue
 							w_coo[0] += 1
 						}
 					}
@@ -164,17 +173,23 @@ fn on_frame(mut app App) {
 					if j != 0 && app.tiles_states[i][j-1] == 0{  // gauche + haut
 						if custom_int_in_range(0,2) == 0{
 							app.tiles_states[i][j]= 0
+							app.screen_pixels[i][j] = 0xFFFF_FFFF
 							app.tiles_states[i][j-1] = 1
+							app.screen_pixels[i][j-1] = blue
 							w_coo[1] -= 1
 						}else{
 							app.tiles_states[i][j]= 0
+							app.screen_pixels[i][j] = 0xFFFF_FFFF
 							app.tiles_states[i+1][j] = 1
+							app.screen_pixels[i+1][j] = blue
 							w_coo[0] += 1
 						}
 					}else{
 						//Aucun des deux, que le haut = descente
 						app.tiles_states[i][j]= 0
+						app.screen_pixels[i][j] = 0xFFFF_FFFF
 						app.tiles_states[i+1][j] = 1
+						app.screen_pixels[i+1][j] = blue
 						w_coo[0] += 1
 					}
 				}
@@ -183,28 +198,38 @@ fn on_frame(mut app App) {
 					if j != 0 && app.tiles_states[i][j-1] == 0{ // deux cotés libres
 						if custom_int_in_range(0,2) == 0{
 							app.tiles_states[i][j]= 0
+							app.screen_pixels[i][j] = 0xFFFF_FFFF
 							app.tiles_states[i][j+1] = 1
+							app.screen_pixels[i][j+1] = blue
 							w_coo[1] += 1
 						}else{
 							app.tiles_states[i][j]= 0
+							app.screen_pixels[i][j] = 0xFFFF_FFFF
 							app.tiles_states[i][j-1] = 1
+							app.screen_pixels[i][j-1] = blue
 							w_coo[1] -= 1
 						}
 					}else{ // que le droite (+)
 						app.tiles_states[i][j]= 0
+						app.screen_pixels[i][j] = 0xFFFF_FFFF
 						app.tiles_states[i][j+1] = 1
+						app.screen_pixels[i][j+1] = blue
 						w_coo[1] += 1
 					}
 				}else{ // pas le droite (+)
 					if j != 0 && app.tiles_states[i][j-1] == 0{  // que le gauche (-)
 						app.tiles_states[i][j]= 0
+						app.screen_pixels[i][j] = 0xFFFF_FFFF
 						app.tiles_states[i][j-1] = 1
+						app.screen_pixels[i][j-1] = blue
 						w_coo[1] -= 1
 					}else{
 						//Aucun des deux
 						if i != 0 && app.tiles_states[i-1][j] == 0 && custom_int_in_range(0,5) == 0{
 							app.tiles_states[i][j]= 0
+							app.screen_pixels[i][j] = 0xFFFF_FFFF
 							app.tiles_states[i-1][j] = 1
+							app.screen_pixels[i-1][j] = blue
 							w_coo[0] -= 1
 						}
 					}
@@ -215,55 +240,21 @@ fn on_frame(mut app App) {
 
     //Draw
 	app.gg.begin()
-	app.gg.draw_rect_filled(x_offset, y_offset-1, pixel_size*nb_tiles, pixel_size*nb_tiles, color(0))
-		app.gg.end(how: .passthru)
-	mut i := 0
-	for (i+1)*refresh < app.water_tiles_coords.len{  // faire new syst avec check si la ligne est dj à jour (la même qu'à la frame d'avant) pi ptet essayer les rectangles de plusieurs pixels
-		app.gg.begin()
-		custom_draw_pixels(app.water_tiles_coords#[i*refresh..i+1*refresh], gx.Color{66, 135, 245, 255}, app.gg)
-		i += 1
-		app.gg.end(how: .passthru)
-	}
-	app.gg.begin()
-	custom_draw_pixels(app.water_tiles_coords#[i*refresh..], gx.Color{66, 135, 245, 255}, app.gg)
-	app.gg.end(how: .passthru)
-
-	mut j := 0
-	for (j+1)*refresh < app.wall_tiles_coords.len{  // New syst où pas de réaffichage du mur sis il était dj là avant
-		app.gg.begin()
-		custom_draw_pixels(app.wall_tiles_coords#[j*refresh..j+1*refresh], gx.black, app.gg)
-		j += 1
-		app.gg.end(how: .passthru)
-	}
-	app.gg.begin()
-	custom_draw_pixels(app.wall_tiles_coords#[j*refresh..], gx.black, app.gg)
-	app.gg.end(how: .passthru)
-	
-	app.gg.begin()
+	app.draw()
 	app.gg.show_fps()
-	app.gg.draw_rect_filled(40, 0, 220, 19, gx.black)
-	app.gg.draw_text(40, 0, "Paint size: ${app.paint_size/2}, Paint type: ${app.paint_type}", text_cfg)
-	app.gg.end(how: .passthru)
+	app.gg.draw_text(40, 0, "Paint size: ${app.paint_size/2}, Paint type: ${app.paint_type}, Nb water particles: ${app.water_tiles_coords.len}", text_cfg)
+	app.gg.end()
 }
 
-[direct_array_access; inline]
-fn custom_draw_pixels(points [][]int, c gx.Color, ctx &gg.Context) {
-	sgl.c4b(c.r, c.g, c.b, c.a)
-	sgl.begin_points()
-	for point in points {
-		x, y := point[0], point[1]
-		sgl.v2f(y * ctx.scale + x_offset, x * ctx.scale + y_offset)
-	}
-	sgl.end()
+fn (mut app App) draw() {
+	mut istream_image := app.gg.get_cached_image_by_idx(app.istream_idx)
+	istream_image.update_pixel_data(unsafe { &u8(&app.screen_pixels) })
+	app.gg.draw_image(x_offset, y_offset, nb_tiles, nb_tiles, istream_image)
 }
 
-[inline]
-fn custom_draw_pixel(x f32, y f32, c gx.Color, ctx &gg.Context) {
-	sgl.c4b(c.r, c.g, c.b, c.a)
 
-	sgl.begin_points()
-	sgl.v2f(x * ctx.scale, y * ctx.scale)
-	sgl.end()
+fn graphics_init(mut app App) {
+	app.istream_idx = app.gg.new_streaming_image(nb_tiles, nb_tiles, 4, pixel_format: .rgba8)
 }
 
 
